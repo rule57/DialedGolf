bfe156c08d67755593061bb83940ea1f60acb93f
rule57 <wjr5232@psu.edu>
2025-12-08 00:59:17 -0500

    Initial Commit

---
+++ DialedGolf/DialedGolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialedGolfApp.swift
+//  DialedGolf
+//
+//  Created by William Rule on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct DialedGolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
